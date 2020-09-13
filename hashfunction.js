b519efe078cee09fa07d06b20f8a0017cb9f765a
Reastrile <71111963+Reastrile@users.noreply.github.com>
2020-09-12 21:47:30 -0500

    Update hashfunction.js

--- hashfunction.js
+++ hashfunction.js
@@ -8,6 +8,10 @@ function hash(text, bit) {
     var Characters = ["A","B","C","D","E","F","G","H","I","J","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","?",">","<",":","}","{","+","_","~","`","!","@","#","$","%","^","&","*","(",")","-","=","[","]",";","'",",",".","/"," "];
     var Text = text.split('');
     var Output = [];
+    var nbit = bit;
+    if (nbit < 5) {
+        nbit = nbit + 7;
+    }
     
     
     for(i = 0 ; i < Text.length ; i++) {
@@ -16,7 +20,7 @@ function hash(text, bit) {
     Output = Output.join('')
     
     while(Output.toString().length < bit) {
-        Output = Math.floor(Output * 3);
+        Output = Math.floor(Output * (nbit / 3));
         Output = BigInt(Output).toString();
     }
     
